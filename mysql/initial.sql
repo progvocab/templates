15dfd9fe71a26430e590bc69be6de68dab356443
progvocab <63547548+progvocab@users.noreply.github.com>
2023-01-13 18:47:57 +0000

    tweek

--- mysql/initial.sql
+++ mysql/initial.sql
@@ -1,1 +1,10 @@
 SHOW DATABASES;
+USE sql6590431;
+
+CREATE TABLE sql6590431.Persons (
+    PersonID int,
+    LastName varchar(255),
+    FirstName varchar(255),
+    Address varchar(255),
+    City varchar(255)
+);
