c4b53165dfcb2d3187315357ac3b29fb96293c14
progvocab <63547548+progvocab@users.noreply.github.com>
2023-01-22 10:29:40 +0000

    mysql

--- mysql/initial.sql
+++ mysql/initial.sql
@@ -8,3 +8,11 @@ CREATE TABLE sql6590431.Persons (
     Address varchar(255),
     City varchar(255)
 );
+
+
+use sql6590431;
+
+INSERT INTO sql6590431.Persons (PersonID, FirstName, LastName, Address, City )
+VALUES ('Cardinal', 'Tom B.', 'Erichsen', 'Skagen 21',  'Norway');
+
+select * from sql6590431.Persons;
