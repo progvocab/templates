5cf1c2337a57f554efb3594879d178d5f176f714
progvocab <63547548+progvocab@users.noreply.github.com>
2023-03-03 15:30:56 +0000

    tweek

---
+++ java/Test.java
@@ -0,0 +1,10 @@
+public class Test {
+    public void display(){
+        System.out.println("HW");
+
+    }
+     public static void main(String args[]){
+        System.out.println("HW");
+        
+    }
+}
